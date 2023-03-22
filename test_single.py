c1ff4ee5e5aeab8927cbf289da80e460e309c496
cgdforever <cgd306948366@163.com>
2023-03-22 22:38:00 +0800

    modified:   test_single.py

--- test_single.py
+++ test_single.py
@@ -10,7 +10,7 @@ collection_list = db.list_collection_names()
 mycol = db["UserChar_new"]
 ids = mycol.distinct("_id")
 
-idstring = "63913f86e9a9d094bc002e6d"
+idstring = "63abe2dc0d5e30820ff6a91e"
 char_id, invalid_label, order_label = model.get_doc(idstring=idstring)
 user_wd = model.get_user(idstring)
 std_wd = model.get_std(char_id)
